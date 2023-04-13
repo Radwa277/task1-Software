46f8bacffcb161fa2cc63bff31056927fc1d3527
Your Name <you@example.com>
2023-04-14 01:48:30 +0200

    first commit

---
+++ resources/views/Task.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+   
+    <head>
+    <meta charset="UTF_8">
+    <meta name="viewport" content="width=device_width,intial-scale=1.0">
+    <title>Task1 Soft</title>
+    </head>
+    <body>
+        <h1>List of names:</h1>
+        <ul>
+            <li>Ahmed</li>
+            <li>Ali</li>
+            <li>Radwa</li>
+            <li>Ebraheim</li>
+            <li>khaled</li>
+            <li>Osama</li>
+            <li>Mohamed</li>
+            <li>Mahmoud</li>
+            <li>Salma</li>
+            <li>Emad</li>
+            <li>Ahmed</li>
+            <li>khaled</li>
+            <li>khaled</li>
+            <li>Ahmed</li>
+            <li>Ali</li>
+            <li>Radwa</li>
+            <li>Ebraheim</li>
+            <li>khaled</li>
+            <li>Osama</li>
+            <li>Mohamed</li>
+            <li>Mahmoud</li>
+            <li>Salma</li>
+            <li>Emad</li>
+            <li>Ahmed</li>
+            <li>khaled</li>
+            <li>khaled</li>
+            <li>Mohamed</li>
+            <li>Mahmoud</li>
+            <li>Salma</li>
+            <li>Emad</li>
+            <li>Ahmed</li>
+            <li>khaled</li>
+            <li>khaled</li>
+            <li>Ahmed</li>
+            <li>Ali</li>
+            <li>Radwa</li>
+            <li>Ebraheim</li>
+            <li>khaled</li>
+            <li>Osama</li>
+            <li>Mohamed</li>
+            <li>Mahmoud</li>
+            <li>Salma</li>
+            <li>Emad</li>
+            <li>Ahmed</li>
+            <li>khaled</li>
+            <li>khaled</li>
+
+        </ul>
+    </body>
+   
+</html>
